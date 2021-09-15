7da96c60b196d15c41880e1fddca5546874b3bff
Nathan <nathanferrer.nf@gmail.com>
2021-09-15 17:36:38 +0200

    fin

--- QuestionnairePractitioner/src/app/home/home.component.ts
+++ QuestionnairePractitioner/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import counterUp from 'counterup2';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  nbHopital: number = 832;
+  nbPractitien: number = 1392;
+
   ngOnInit(): void {
+    const el = document.querySelector( '.counter' );
+    const el2 = document.querySelector( '.counter2' );
+
+    counterUp( el, {
+      duration: 3000,
+      delay: 16,
+    } );
+    counterUp( el2  , {
+      duration: 3000,
+      delay: 16,
+    } );
   }
 }
